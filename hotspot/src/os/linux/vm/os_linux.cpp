fe79026caa064a458535508d477b38116f15ff63
Rokkubro <lachlanb03@gmail.com>
2020-03-19 23:39:07 +1100

    Fix os_linux.cpp to work with gcc 9

--- hotspot/src/os/linux/vm/os_linux.cpp
+++ hotspot/src/os/linux/vm/os_linux.cpp
@@ -4951,7 +4951,9 @@ void os::Linux::check_signal_handler(int sig) {
   }
 
   if (thisHandler != jvmHandler) {
-    tty->print("Warning: %s handler ", exception_name(sig, buf, O_BUFLEN));
+    const char* ename = exception_name(sig, buf, O_BUFLEN);
+    if(ename)
+      tty->print("Warning: %s handler ", ename);
     tty->print("expected:%s", get_signal_handler_name(jvmHandler, buf, O_BUFLEN));
     tty->print_cr("  found:%s", get_signal_handler_name(thisHandler, buf, O_BUFLEN));
     // No need to check this sig any longer
